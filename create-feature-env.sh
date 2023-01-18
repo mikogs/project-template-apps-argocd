0f30b7669e8c594d8d290188d41265a425648ccb
Mikolaj Gasior <mikolaj@gasior.dev>
2023-01-18 07:20:47 +0100

    Add create-feature-env.sh

---
+++ create-feature-env.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [[ "$1" == "" ]]; then
+  echo "Syntax: create-feature-env.sh ENV_NAME"
+  exit 1
+fi
+
+cat >devenv/argocd/overlays/devenv/region/fe-$1.yaml <<EOF
+---
+apiVersion: argoproj.io/v1alpha1
+kind: Application
+metadata:
+  name: fe-$1-node
+  namespace: devenv-argocd
+spec:
+  project: default
+  source:
+    repoURL: https://github.com/mikogs/project-template-envs.git
+    path: featureenvs/$1/
+    targetRevision: HEAD
+  destination:
+    server: 'https://kubernetes.default.svc'
+    namespace: fe-$1
+  syncPolicy:
+    automated: {}
+    syncOptions:
+      - CreateNamespace=true
+
+EOF
+cd devenv/argocd/overlays/devenv/region
+kustomize edit add resource fe-$1.yaml
